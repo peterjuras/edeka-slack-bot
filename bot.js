1ab55bda23b62938011d0dbb0e88f5fdd84ed3ce
Peter Juras <p.juras@outlook.com>
2016-02-08 19:29:17 +0100

    [WIP] menu parsing

--- bot.js
+++ bot.js
@@ -1,19 +1,19 @@
-const Botkit = require('botkit');
+// const Botkit = require('botkit');
 const edeka = require('./edeka');
-const controller = Botkit.slackbot();
-const bot = controller.spawn({
-  token: process.env.SLACK_BOT_TOKEN
-});
+// const controller = Botkit.slackbot();
+// const bot = controller.spawn({
+  // token: process.env.SLACK_BOT_TOKEN
+// });
 
-bot.startRTM((error, bot, payload) => {
-  if (error) {
-    throw new Error('Could not connect to Slack');
-  }
-});
-
-controller.hears('Edeka', 'direct_message,direct_mention,mention,ambient', (bot,message) => {
-  bot.reply(message, 'Hi');
-});
+// bot.startRTM((error, bot, payload) => {
+//   if (error) {
+//     throw new Error('Could not connect to Slack');
+//   }
+// });
+//
+// controller.hears('Edeka', 'direct_message,direct_mention,mention,ambient', (bot,message) => {
+//   bot.reply(message, 'Hi');
+// });
 
 edeka.getMenuForDay((error, result) => {
   console.log(error);
--- edeka.js
+++ edeka.js
@@ -1,10 +1,53 @@
 const htmlparser = require('htmlparser');
+const moment = require('moment');
 const request = require('request');
 const select = require('soupselect').select;
 
-const menu_url = 'http://simmel.de/wochenmenue/muenchen';
+const menu_url = 'http://simmel.de/wochenmenue/dresden';
+// const menu_url = 'http://simmel.de/wochenmenue/muenchen';
 
-function getMenuForDay(callback) {
+const dateRegex = /\d\d[.]\d\d[.]\d\d/g;
+const dateFormat = 'DD.MM.YYYY';
+
+function parseMeals(rawMeals) {
+  const meals = [];
+  rawMeals.forEach(rawMeal => {
+    const meal = {};
+    meal.meal = select(rawMeal, 'div.value')[0].children[0].data;
+    meal.price = select(rawMeal, 'div.price')[0].children[0].data;
+    meals.push(meal);
+  });
+  return meals;
+}
+
+function parseDays(rawDays) {
+  const days = [];
+  rawDays.forEach(rawDay => {
+    const day = {};
+    const dayString = select(rawDay, 'h5')[0].children[0].data;
+    const dayDate = dayString.match(dateRegex)[0];
+    day.date = moment(dayDate, dateFormat);
+    const rawMeals = select(rawDay, 'div.item');
+    day.meals = parseMeals(rawMeals);
+    days.push(day);
+  });
+  return days;
+}
+
+function parseWeek(rawWeek) {
+  const week = {};
+  const weekString = select(rawWeek, 'h4')[0].children[0].data;
+  const weekDates = weekString.match(dateRegex);
+  week.startDay = moment(weekDates[0], dateFormat);
+  week.endDay = moment(weekDates[1], dateFormat);
+
+  const rawDays = select(rawWeek, 'div.market-menu');
+  week.days = parseDays(rawDays);
+
+  return week;
+}
+
+function getWeeks(callback) {
   request(menu_url, (error, response, body) => {
     if (error || response.statusCode !== 200) {
       callback(error || response.statusCode);
@@ -15,12 +58,31 @@ function getMenuForDay(callback) {
         callback(error);
         return;
       }
-      const weeks = select(dom, 'div.element');
-      weeks.forEach(console.log);
+      const rawWeeks = select(dom, 'div.element');
+      callback(null, rawWeeks.map(parseWeek));
     });
     const parser = new htmlparser.Parser(handler);
     parser.parseComplete(body);
-  })
+  });
+}
+
+function isWithinWeek(day, week) {
+  return day.isBetween(week.startDay, week.endDay);
+}
+
+function getMenuForDay(callback) {
+  getWeeks((error, weeks) => {
+    if (error) {
+      callback(error);
+      return;
+    }
+    const today = moment();
+    weeks.forEach(week => {
+      if (isWithinWeek(today, week)) {
+        return getMenuFromDayAndWeek(today, week);
+      }
+    })
+  });
 }
 
 module.exports = {
